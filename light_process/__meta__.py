710adbee173f46f69e58d9c2308a4db71bee7507
justengel <jtengel08@gmail.com>
2020-12-07 11:37:40 -0500

    Updated to version 0.0.6

--- light_process/__meta__.py
+++ light_process/__meta__.py
@@ -1,5 +1,5 @@
 name = 'light_process'
-version = '0.0.5'
+version = '0.0.6'
 description = 'Python multiprocessing.Process that does not need to import __main__.'
 url = 'https://github.com/justengel/light_process'
 author = 'Justin Engel'
